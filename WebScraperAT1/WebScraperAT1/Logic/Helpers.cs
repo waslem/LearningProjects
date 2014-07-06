300316216b5b9decc5178e11ddee80fe9c0dc02b
waslem <jvw@westnet.com.au>
2014-07-06 11:21:32 +0800

    created a helper class to get website source created helper method to get website links from source

---
+++ WebScraperAT1/WebScraperAT1/Logic/Helpers.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebScraperAT1.Logic
+{
+    public static class Helpers
+    {
+        /// <summary>
+        /// retrieves the specified website's source code
+        /// </summary>
+        /// <param name="url">the website we wish to get source for</param>
+        /// <returns>string containing source code</returns>
+        public static string GetWebsiteSource(string url)
+        {
+
+            // create the request
+            WebRequest request = HttpWebRequest.Create(url);
+
+            // get the response from the server
+            WebResponse response = request.GetResponse();
+
+            Stream stream = response.GetResponseStream();
+
+            StreamReader reader = new StreamReader(stream);
+
+            string result = reader.ReadToEnd();
+
+            return result;
+        }
+
+        /// <summary>
+        /// finds all html links in a websites source
+        /// </summary>
+        /// <param name="source">source code of website</param>
+        /// <returns>list of strings containing links</returns>
+        internal static List<string> FindWebsiteLinks(string source)
+        {
+            string[] separator = { "\r\n" };
+
+            // split at \r\n
+            var lines = source.Split(separator,10000, StringSplitOptions.RemoveEmptyEntries);
+
+            List<string> links = new List<string>();
+
+            foreach (var line in lines)
+            {
+                if (line.Contains("<a href="))
+                {
+                    links.Add(line.Trim());
+                }
+            }
+
+            return links;
+        }
+    }
+}
---
+++ WebScraperAT1/WebScraperAT1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using WebScraperAT1.Logic;
+
+namespace WebScraperAT1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string source = Helpers.GetWebsiteSource("http://www.baycorp.com.au");
+
+            List<string> links = Helpers.FindWebsiteLinks(source);
+
+            foreach (var link in links)
+            {
+                Console.WriteLine(link);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
